c4b9b5921e51fadac42d460602b69b44b3aacf88
KaiqueSM <kaiquesmend@gmail.com>
2021-07-26 23:51:22 -0300

    adição do Toast, da cor laranja e criação de interface(classe) para filme

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
+import { IFilme } from './../models/IFilme.models';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -8,7 +10,27 @@ import { AlertController } from '@ionic/angular';
 })
 export class Tab1Page {
 
-  constructor(public alertController: AlertController) {}
+  titulo = 'Vídeos App';
+  listaVideos: IFilme[] = [
+    {
+      nome: 'Mortal Kombat (2021)',
+      lancamento: '15/04/2021',
+      duracao: '1h 50m',
+      classificacao: 76,
+      generos: ['Ação', 'Aventura', 'Fantasia'],
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ijvC2w2yANsfgLT3LMu2zFr0fxh.jpg'
+    },
+    {
+      nome: 'O Senhor das Armas (2005)',
+      lancamento: '14/10/2005',
+      duracao: '2h 2m',
+      classificacao: 73,
+      generos: ['Crime', 'Drama', 'Thriller'],
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/yLxbDZ1h7wbHo7mkyX5AR9hjODe.jpg'
+    }
+  ];
+
+  constructor(public alertController: AlertController, public toastController: ToastController) {}
 
   async exibirAlertaFavorito() {
     const alert = await this.alertController.create({
@@ -24,13 +46,22 @@ export class Tab1Page {
         }, {
           text: 'Sim',
           handler: () => {
-            console.log('Confirm Okay');
+            this.exibirToast();
           }
         }
       ]
     });
 
     await alert.present();
+  }
+
+  async exibirToast() {
+    const toast = await this.toastController.create({
+      message: 'Adicionado aos favoritos!',
+      duration: 2000,
+      color: 'success'
+    });
+    toast.present();
   }
 
 }
